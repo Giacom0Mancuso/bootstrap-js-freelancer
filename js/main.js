c9739590e04bedfd487d39b866e25f5c15f1d333
Giacom0Mancuso <giacomomancuso35@email.com>
2023-11-27 11:36:29 +0100

    error not found

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
 let typeOfWork = document.getElementById("type-of-work").value;
 let valueNumOfHours = document.getElementById("hours").value;
-let NumOfHours = parseFloat(valueNumOfhours.value);
+let NumOfHours = parseFloat(valueNumOfHours);
 let arreyDiscount = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
-let discountNotUsed = new Array(arreyDiscount.length).fill(true);
+let discountNotUsed = [true, true, true, true, true]
 let chooseDiscount = document.getElementById("discount-code").value;
-let totalNotDiscountPrice=0;
+let notDiscountPrice=0.00;
+let disconuntedPrice = 0.00;
 
-if(typeOfWork == 1){
-    totalNotDiscountPrice = NumOfHours * 20.50;
+if(typeOfWork.value == 1){
+    notDiscountPrice = NumOfHours * 20.50;
 }
-else if(typeOfWork == 2){
-    totalNotDiscountPrice = NumOfHours * 15.30;
+else if(typeOfWork.value == 2){
+    notDiscountPrice = NumOfHours * 15.30;
 }
 else{
-    totalNotDiscountPrice = NumOfHours * 33.60;
+    notDiscountPrice = NumOfHours * 33.60;
 }
 
+
+
+for(let i = 0; i < arreyDiscount.length; i++){
+    if (!discountNotUsed){alert("Ah furbetto hai provato a inserire di nuovo il codice eh!")}
+    else{
+    if(chooseDiscount == arreyDiscount[i]){
+        disconuntedPrice = notDiscountPrice - (notDiscountPrice * 25 / 100);
+        document.getElementById("original_price").innerHTML = notDiscountPrice.toFixed(2);
+        document.getElementById("final_price").innerHTML = discountedPrice.toFixed(2);
+        discountNotUsed[i] = false;
+    }
+}
+}
+
+let checkDiscount = true;
+
+for(let i = 0; i < discountNotUsed.length; i++){
+     if(discountNotUsed[i] ==false){
+        checkDiscount = false; 
+     }
+}
+
+if (checkDiscount == true){
+    document.getElementById("final_price").innerHTML = notDiscountPrice.toFixed(2);
+        document.getElementById("original_price").innerHTML = "";
+}
